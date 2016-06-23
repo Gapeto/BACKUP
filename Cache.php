01de9207722033d15c52aceeb47f82942b963e35
Gapeto <mjackson@gapeto.com>
2016-06-23 12:01:54 -0400

    Create Cache.php

---
+++ Cache.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Cache
+Description: Controls all Cache
+Author: Gapeto LLC
+Version: 1.0
+Author URI: https://www.gapeto.com/
+*/
+require WPMU_PLUGIN_DIR.'/Cache/my-plugin.php';
