a425bffc5b0e017a7e4d82c06477bd3ee09bfd3d
Gapeto <mjackson@gapeto.com>
2016-06-23 11:58:18 -0400

    Create Redirects.php

---
+++ Redirects.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Redirects
+Description: Controls redirects
+Author: Gapeto LLC
+Version: 1.0
+Author URI: https://www.gapeto.com/
+*/
+require WPMU_PLUGIN_DIR.'/Redirects/my-plugin.php';
