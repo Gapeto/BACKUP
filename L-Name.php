0090bd64318aaa0442d3d0b8d7fb3ccc77bd733e
Gapeto <mjackson@gapeto.com>
2016-06-23 11:57:20 -0400

    Create L-Name.php

---
+++ L-Name.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: L-Name.php
+Description: Controls the L-Name
+Author: Gapeto LLC
+Version: 1.0
+Author URI: https://www.gapeto.com/
+*/
+require WPMU_PLUGIN_DIR.'/L-Name/my-plugin.php';
