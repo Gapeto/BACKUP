fd0a1459b690fb1823241c3b072cfd3d2c8bbe42
Gapeto <mjackson@gapeto.com>
2016-06-23 11:54:56 -0400

    Create Stats.php

---
+++ Stats.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Stats
+Description: Controls site stats
+Author: Gapeto LLC
+Version: 1.0
+Author URI: https://www.gapeto.com/
+*/
+require WPMU_PLUGIN_DIR.'/Stats/my-plugin.php';
