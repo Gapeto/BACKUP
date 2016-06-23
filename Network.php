67a2b773ee1105d9cb80c8d1056e47b801c91042
Gapeto <mjackson@gapeto.com>
2016-06-23 11:45:08 -0400

    Create Network.php

---
+++ Network.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Network
+Description: Controls site creation
+Author: Gapeto LLC
+Version: 1.0
+Author URI: https://www.gapeto.com/
+*/
+require WPMU_PLUGIN_DIR.'/Network/my-plugin.php';
