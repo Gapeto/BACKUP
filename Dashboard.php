81432a9256bb94cee5dfe0bd41c22d50ea13c6e3
Gapeto <mjackson@gapeto.com>
2016-06-23 11:56:05 -0400

    Create Dashboard.php

---
+++ Dashboard.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Dashboard
+Description: Controls welcome dashboard
+Author: Gapeto LLC
+Version: 1.0
+Author URI: https://www.gapeto.com/
+*/
+require WPMU_PLUGIN_DIR.'/Dashboard/my-plugin.php';
