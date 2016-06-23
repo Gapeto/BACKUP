d89c32917c823733c6364390af45cb42cb281e3b
Gapeto <mjackson@gapeto.com>
2016-06-23 11:52:57 -0400

    Create Mapper.php

---
+++ Mapper.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Mapper
+Description: Controls External domains
+Author: Gapeto LLC
+Version: 1.0
+Author URI: https://www.gapeto.com/
+*/
+require WPMU_PLUGIN_DIR.'/Mapper/my-plugin.php';
