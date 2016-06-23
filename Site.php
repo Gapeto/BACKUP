97eeb3cbf00674abdd1f412e0f605528822b7027
Gapeto <mjackson@gapeto.com>
2016-06-23 11:59:31 -0400

    Create Site.php

---
+++ Site.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Site
+Description: Creates site settings
+Author: Gapeto LLC
+Version: 1.0
+Author URI: https://www.gapeto.com/
+*/
+require WPMU_PLUGIN_DIR.'/Site/my-plugin.php';
